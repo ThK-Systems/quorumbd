329410685b2cf547688fea19a76f783aee066bdb
Thomas Kuhlmann <thomas.kuhlmann@thk-systems.de>
2026-03-24 17:35:47 +0100

    Real control message

--- common/control/controlmessage.go
+++ common/control/controlmessage.go
@@ -1,4 +1,20 @@
 // Package control provides messages for the control plane
 package control
 
-type ControlMessage interface{}
+import "fmt"
+
+const (
+	CMDummy = iota
+)
+
+type ControlMessage interface {
+	Type() uint32
+	RequestID() uint64
+}
+
+func NewMessage(messageType uint32) (ControlMessage, error) {
+	switch messageType {
+	default:
+		return nil, fmt.Errorf("unknown type %d", messageType)
+	}
+}
