25864c3197da83b47eb5c868879bc8cadc90f117
Thomas Kuhlmann <thomas.kuhlmann@thk-systems.de>
2026-03-25 15:35:07 +0100

    Added `BaseControlMessage` as `struct` with json `tags`

--- common/control/controlmessage.go
+++ common/control/controlmessage.go
@@ -1,7 +1,9 @@
 // Package control provides messages for the control plane
 package control
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const (
 	CMDummy = iota
@@ -11,6 +13,16 @@ type ControlMessage interface {
 	Type() uint32
 	RequestID() uint64
 	isResponse() bool
+	GeneratedAt() uint64 // Milliseconds since 1970
+	GeneratedBy() uint32 // UUID of instance
+}
+
+type BaseControlMessage struct {
+	Type        uint32 `json:"type"`
+	RequestID   uint64 `json:"request_id"`
+	IsResponse  bool   `json:"is_response"`
+	GeneratedAt uint64 `json:"generated_at"`
+	GeneratedBy uint32 `json:"generated_by"`
 }
 
 func NewMessage(messageType uint32) (ControlMessage, error) {
